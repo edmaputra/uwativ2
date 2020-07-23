3a6e407a9696f2e008907ea8a03d0bf10826e260
Bangun Edma <bangun.edma.saputra@gmail.com>
2020-07-23 22:12:27 +0700

    Change project files

--- patient/src/main/java/io/github/edmaputra/uwati/patient/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package io.github.edmaputra.uwati.patient.repository;
-
-import io.github.edmaputra.uwati.patient.domain.Address;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface AddressRepository extends ReactiveCrudRepository<Address, String> {
-}
--- patient/src/main/java/io/github/edmaputra/uwati/patient/repository/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package io.github.edmaputra.uwati.patient.repository;
-
-import io.github.edmaputra.uwati.patient.domain.Patient;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface PatientRepository extends ReactiveCrudRepository<Patient, String> {
-}
